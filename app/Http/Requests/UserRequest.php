0efe94e84c91a74a1478915a694955b46f3d16b2
Sian Chou <webcodedemo@gmail.com>
2019-02-10 19:54:07 +0000

    限制頭像上傳大小

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -18,12 +18,15 @@ class UserRequest extends FormRequest
             'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' . Auth::id(),
             'email' => 'required|email',
             'introduction' => 'max:80',
+            'avatar' => 'mimes:jpeg,bmp,png,gif|dimensions:min_width=208,min_height=208',
         ];
     }
 
     public function messages()
     {
         return [
+            'avatar.mimes' => '頭像必須是 jpeg, bmp, png, gif 格式的圖片',
+            'avatar.dimensions' => '圖片的解析度不夠，寬和高需要208px以上',
             'name.unique' => '使用者名稱已經被使用，請重新填寫',
             'name.regex' => '使用者名稱只支援英文、數字、橫槓、下底線',
             'name.between' => '使用者名稱必須介於 3- 25字元之間',
