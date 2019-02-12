47ab01acf80b4c5bd285057f55500cf351020643
Sian Chou <webcodedemo@gmail.com>
2019-02-12 06:32:51 +0000

    分類主題列表

---
+++ app/Http/Controllers/CategoriesController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Topic;
+use App\Models\Category;
+
+class CategoriesController extends Controller
+{
+    public function show(Category $category)
+    {
+        // 讀取分類 ID 關聯的主題，並且每 20 條為一個分頁
+        $topics = Topic::where('category_id', $category->id)->paginate(20);
+
+        // 傳遞主題和分類的變數到模板中
+        return view('topics.index', compact('topics', 'category'));
+    }
+}
