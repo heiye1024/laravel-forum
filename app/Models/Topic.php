594070c720d0eda869386ff993bfbfa7a895faf7
Sian Chou <webcodedemo@gmail.com>
2019-02-12 02:20:05 +0000

    主題列表頁面

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -4,5 +4,20 @@ namespace App\Models;
 
 class Topic extends Model
 {
-    protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+    protected $fillable = [
+        'title', 'body', 'user_id', 'category_id', 'reply_count',
+        'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'
+    ];
+
+    // 一個主題屬於一個分類
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    // 一個主題擁有一個作者
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
