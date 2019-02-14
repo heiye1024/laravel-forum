6ed20c195f8ae5102fd81ed7f73a86526ac6b019
Sian Chou <webcodedemo@gmail.com>
2019-02-14 00:23:58 +0000

    使用者可以編輯主題

--- app/Policies/TopicPolicy.php
+++ app/Policies/TopicPolicy.php
@@ -7,10 +7,10 @@ use App\Models\Topic;
 
 class TopicPolicy extends Policy
 {
+    // 只有當主題的作者ID 等於 目前登入的使用者ID，才可以進行更新主題
     public function update(User $user, Topic $topic)
     {
-        // return $topic->user_id == $user->id;
-        return true;
+        return $topic->user_id == $user->id;
     }
 
     public function destroy(User $user, Topic $topic)
--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -34,7 +34,8 @@
                 <select class="form-control" name="category_id" required>
                   <option value="" hidden disabled selected>請選擇分類</option>
                   @foreach ($categories as $value)
-                    <option value="{{$value->id}}">{{$value->name}}</option>
+                    <option value="{{$value->id}}" {{$topic->category_id == $value->id ? 'selected' : ''}}>
+                      {{$value->name}}
                   @endforeach
                 </select>
               </div>
