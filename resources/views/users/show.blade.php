bc010d7eb63062907657f7a3d63b450c43088199
Sian Chou <webcodedemo@gmail.com>
2019-02-15 03:20:21 +0000

    回覆列表

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -22,13 +22,18 @@
       </div>
     </div>
     <hr>
+    {{--使用者發佈的內容--}}
     <div class="card ">
       <div class="card-body">
         <ul class="nav nav-tabs">
-          <li class="nav-item"><a class="nav-link active bg-transparent" href="#">我的主題</a></li>
-          <li class="nav-item"><a class="nav-link" href="#">我的回覆</a></li>
+          <li class="nav-item"><a class="nav-link active bg-transparent {{active_class(if_query('tab', null))}}" href="{{route('users.show', $user->id)}}">我的主題</a></li>
+          <li class="nav-item"><a class="nav-link bg-transparent {{active_class(if_query('tab', 'replies'))}}" href="{{route('users.show', [$user->id, 'tab' => 'replies'])}}">我的回覆</a></li>
         </ul>
-        @include('users._topics', ['topics' => $user->topics()->recent()->paginate(5)])
+        @if (if_query('tab', 'replies'))
+          @include('users._replies', ['replies' => $user->replies()->with('topic')->recent()->paginate(5)])
+        @else
+          @include('users._topics', ['topics' => $user->topics()->recent()->paginate(5)])
+        @endif
       </div>
     </div>
   </div>
